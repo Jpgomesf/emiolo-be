b3801c19a28f882d712f16bd2787e36dc5bcfd56
jpgomesf <jppromus+dev@gmail.com>
2023-03-14 18:51:49 -0300

    (emiolo-be) Refactoring of: UserController and UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, NotFoundException, InternalServerErrorException, HttpStatus, HttpCode } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user.schema';
 import { CreateUserDto } from './dto/user.dto';
@@ -8,13 +8,34 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    const { email, name, photo } = createUserDto;
-    return this.userService.createUser(email, name, photo);
+    const { email } = createUserDto;
+    const existingUser = await this.userService.findUserByEmail(email);
+
+    if (existingUser) {
+      const updatedUser = await this.userService.updateUser(existingUser._id, createUserDto);
+      return updatedUser;
+    } else {
+      try {
+        const user = await this.userService.createUser(createUserDto);
+        return user;
+      } catch (error) {
+        throw new InternalServerErrorException('Failed to create user');
+      }
+    }
   }
 
   @Get()
-  async getUsers() {
-    return this.userService.getUsers();
+  async getUsers(): Promise<User[]> {
+    try {
+      const users = await this.userService.getUsers();
+      if (users.length === 0) {
+        throw new NotFoundException('No users found');
+      }
+      return users;
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to get users');
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../user/schema/user.schema';
+import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async createUser(email: string, name: string, photo: string): Promise<User> {
-    const user = new this.userModel({
-      email,
-      name,
-      photo,
-    });
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
 
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user = new this.userModel(createUserDto);
     return await user.save();
   }
 
   async getUsers(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+
+  async updateUser(userId: string, updateUserDto: CreateUserDto): Promise<User> {
+    const updatedUser = await this.userModel.findByIdAndUpdate(
+      userId,
+      {
+        $set: updateUserDto,
+      },
+      {
+        new: true,
+        runValidators: true,
+      },
+    ).exec();
+
+    if (!updatedUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    return updatedUser;
   }
 }
