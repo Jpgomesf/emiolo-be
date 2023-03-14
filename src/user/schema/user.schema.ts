fcdfc187296ca71a2e2a6aec6979ae49f12eec26
jpgomesf <jppromus+dev@gmail.com>
2023-03-14 18:02:24 -0300

    (emiolo-be) Created User DTO and Schema

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+    timestamps: true,
+})
+export class User extends Document {
+    @Prop({
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: /^[a-zA-Z0-9._%+-]+([a-zA-Z0-9._%+-]+)*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+        trim: true,
+        lowercase: true,
+      })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+  })
+  name: string;
+
+  @Prop()
+  photo: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
